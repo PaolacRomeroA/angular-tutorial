9fe0ec81f91ad5507d0ea279f7dec3ccf6ee8cff
Paola Romero <pcra95@gmail.com>
2024-09-26 12:06:04 +0200

    tutorial de angular

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent)
+import { provideRouter } from '@angular/router';
+import routeConfig from './app/routes';
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routeConfig)
+  ]
+})
   .catch(err => console.error(err));
